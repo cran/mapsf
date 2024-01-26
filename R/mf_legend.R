2f3bfdda276dc3ff879cd5e7493339c02ffdd3b6
Timothée Giraud <timothee.giraud@cnrs.fr>
2024-01-26 12:40:03 +0000

    version 0.9.0

--- R/mf_legend.R
+++ R/mf_legend.R
@@ -139,14 +139,14 @@ mf_legend <- function(type,
                       pt_cex_na,
                       pt_pch_na) {
   test_cur_plot()
-  args <- as.list(match.call())
-  args <- args[-1]
+  args <- match.call()
   args$mar <- getOption("mapsf.mar")
   if (missing(bg)) args$bg <- getOption("mapsf.bg")
   if (missing(fg)) args$fg <- getOption("mapsf.fg")
   if (missing(frame_border)) args$frame_border <- getOption("mapsf.fg")
   if (type == "symb") args <- clean_symb_args(args)
-  r <- do.call(leg, args)
+  args[[1]] <- quote(maplegend::leg)
+  r <- eval.parent(args)
   return(invisible(r))
 }
 
